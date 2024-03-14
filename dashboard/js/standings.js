4861a76f5e3f223764aabebb008f1f3c02338f30
Jeremy Schuler <j.waltenspuehl@weissauned.ch>
2024-03-14 11:00:25 +0100

    modified:   dashboard/css/standings.css 	modified:   dashboard/js/standings.js 	modified:   dashboard/standings.html 	new file:   extension/assets/updater/Updater.js 	new file:   extension/assets/updater/Updater.min.js 	modified:   extension/index.js

--- dashboard/js/standings.js
+++ dashboard/js/standings.js
@@ -1,3 +1,24 @@
 // You can access the NodeCG api anytime from the `window.nodecg` object
 // Or just `nodecg` for short. Like this!:
-nodecg.log.info("Here's an example of using NodeCG's logging API!");
+const rpl_GraphicsUpdaterRequirements = nodecg.Replicant('GraphicsUpdaterRequirements');
+const rpl_PlayerName = nodecg.Replicant('PlayerName')
+function update_Values(){
+    rpl_GraphicsUpdaterRequirements.value.spreadsheetID = document.getElementById('in_spreadsheetId').value
+    rpl_GraphicsUpdaterRequirements.value.worksheetName = document.getElementById('in_worksheetName').value
+    rpl_GraphicsUpdaterRequirements.value.apiKey = document.getElementById('in_apiKey').value
+    rpl_GraphicsUpdaterRequirements.value.updateInterval = document.getElementById('in_updateInterval').value
+}
+
+function update_PlayerName() {
+    rpl_PlayerName.value = document.getElementById('in_playername').value
+}
+
+rpl_GraphicsUpdaterRequirements.on('change', () => {
+    document.getElementById('in_spreadsheetId').value = rpl_GraphicsUpdaterRequirements.value.spreadsheetID
+    document.getElementById('in_worksheetName').value = rpl_GraphicsUpdaterRequirements.value.worksheetName
+    document.getElementById('in_apiKey').value = rpl_GraphicsUpdaterRequirements.value.apiKey
+    document.getElementById('in_updateInterval').value = rpl_GraphicsUpdaterRequirements.value.updateInterval
+})
+rpl_PlayerName.on('change', () => {
+    document.getElementById('in_playername').value = rpl_PlayerName.value
+})
--- extension/index.js
+++ extension/index.js
@@ -1,3 +1,13 @@
 module.exports = function (nodecg) {
-
+	nodecg.Replicant('GraphicsUpdaterRequirements', {defaultValue: 
+		{
+			spreadsheetID: "Spreadsheet ID",
+			worksheetName: "Worksheet Name",
+			apiKey: "api Key",
+			updateInterval: 3000
+		}
+	});
+	nodecg.Replicant('PlayerName', {
+		defaultValue: "VG | NakaJF"
+	})
 };
